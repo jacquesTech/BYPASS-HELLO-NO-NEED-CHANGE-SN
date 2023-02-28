687d2dd0ad941934b682cb03eaf4bcfef27f8002
GSM ADJAA <96638829+jacquesTech@users.noreply.github.com>
2023-02-28 23:28:23 +0100

    Update GenerateFiles.sh

--- GenerateFiles.sh
+++ GenerateFiles.sh
@@ -7,10 +7,10 @@ Devi(){ ./ideviceinfo | grep -w $1 | awk '{printf $NF}'; };
 PLutil(){ echo -e $1 >>$2; };
 
 chmod +x ideviceinfo
-printf "making activation_record.plist"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt ".";
+printf "generating activation record"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt ".";
 curl -s -k "https://gsmadjaa.x10.bz/helloserver/activation.php?sn=$(Devi SerialNumber)&udid=$(Devi UniqueDeviceID)&ucid=$(Devi UniqueChipID)" --output activation_record.plist
 echo ''
 printf "making IC-Info.sisv"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt ".";
 curl -s -k "https://gsmadjaa.x10.bz/helloserver/Devices/activation_records/$(Devi UniqueDeviceID)/IC-Info.sisv" --output IC-Info.sisv
 echo ''
-echo "DONE!"
+echo "FILES SUCCESSFULLY GENERATED"
